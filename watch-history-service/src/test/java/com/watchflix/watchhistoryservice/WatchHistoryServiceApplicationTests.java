f471ed42a268c827fed850a9be552d62012b7ffe
Liandany,Rebeka R.D <r.liandany@student.fontys.nl>
2022-11-02 22:00:37 +0100

    add watch history service

---
+++ watch-history-service/src/test/java/com/watchflix/watchhistoryservice/WatchHistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watchflix.watchhistoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchHistoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
