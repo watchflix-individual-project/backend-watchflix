1c483d13b8c739af7cdcc3865a9115840e89a73f
Liandany,Rebeka R.D <r.liandany@student.fontys.nl>
2022-11-02 22:01:17 +0100

    add config server

---
+++ configuration-server/src/test/java/com/watchflix/configurationserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watchflix.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
